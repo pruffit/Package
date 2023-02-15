5753710f6eea1c2352855f7d5e3c0e309585a132
Danil Kotlaev <kotlaevdanil@gmail.com>
2023-02-16 03:59:45 +0400

    tasks copy and clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-const { src, dest } = require('gulp')
+const { src, dest, task, series } = require('gulp')
+const rm = require('gulp-rm')
 
-function copy() {
-	return src('src/styles/main.scss').pipe(dest('dist'))
-}
+task('clean', () => {
+	return src('dist/**/*', {read: false}).pipe(rm())
+})
 
-exports.copy = copy
+task('copy', series('clean',  () => {
+	return src('src/**/*.scss').pipe(dest('dist'))
+}))
