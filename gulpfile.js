41003e27fab18b576ed931ad635ccaee78c59fbc
Danil Kotlaev <kotlaevdanil@gmail.com>
2023-02-16 05:08:22 +0400

    local server

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ const { src, dest, task, series, watch } = require("gulp");
 const rm = require('gulp-rm');
 const sass = require('gulp-sass')(require('node-sass'));
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
  
 task('clean', () => {
  	return src('dist/**/*', { read: false })
-   	.pipe(rm())
-})
+   	.pipe(rm());
+});
  
-task('copy', () => {
- 	return src('src/styles/*.scss').pipe(dest('dist'));
-})
+task('copy:html', () => {
+ 	return src('src/*.html').pipe(dest('dist')).pipe(reload({stream: true}));
+});
  
 task('styles', () => {
  	return src(['node_modules/normalize.css/normalize.css', 'src/styles/main.scss'])
@@ -18,7 +20,17 @@ task('styles', () => {
    	.pipe(sass().on('error', sass.logError))
    	.pipe(dest('dist'));
 });
- 
-watch('./src/styles/**/*.scss', series('styles'))
 
-task('default', series('clean', 'styles'));
+task('server', () => {
+	browserSync.init({
+		server: {
+			baseDir: './dist'
+		},
+		open: false
+	});
+});
+ 
+watch('./src/styles/**/*.scss', series('styles'));
+watch('./src/*.html', series('copy:html'));
+
+task('default', series('clean', 'copy:html', 'styles', 'server'));
