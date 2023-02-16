45c63cddf0e4f75d7a4558e9864c6187af3f7623
Danil Kotlaev <kotlaevdanil@gmail.com>
2023-02-16 04:20:32 +0400

    task scss compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const { src, dest, task, series } = require('gulp')
 const rm = require('gulp-rm')
+const sass = require('gulp-sass')(require('node-sass'))
 
 task('clean', () => {
 	return src('dist/**/*', {read: false}).pipe(rm())
 })
 
-task('copy', series('clean',  () => {
+task('copy', () => {
 	return src('src/**/*.scss').pipe(dest('dist'))
-}))
+})
+
+task('styles', () => {
+	return src('src/styles/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(dest('dist'))
+})
+
+task ('default', series('clean', 'styles'))
