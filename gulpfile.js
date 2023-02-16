72719143bb35f03e61c70ed913ff607a10d15973
Danil Kotlaev <kotlaevdanil@gmail.com>
2023-02-16 06:14:15 +0400

    glob and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass')(require('node-sass'));
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+const autoprafixer = require('gulp-autoprefixer');
  
 task('clean', () => {
  	return src('dist/**/*', { read: false })
@@ -17,7 +19,12 @@ task('copy:html', () => {
 task('styles', () => {
  	return src(['node_modules/normalize.css/normalize.css', 'src/styles/main.scss'])
    	.pipe(concat('main.scss'))
+		.pipe(sassGlob())
    	.pipe(sass().on('error', sass.logError))
+		.pipe(autoprafixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
    	.pipe(dest('dist'));
 });
 
