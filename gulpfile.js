1d372025345efea06f4c43a52deaf06c088321c4
Danil Kotlaev <kotlaevdanil@gmail.com>
2023-02-16 04:46:19 +0400

    watch and concat styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
-const { src, dest, task, series } = require('gulp')
-const rm = require('gulp-rm')
-const sass = require('gulp-sass')(require('node-sass'))
-
+const { src, dest, task, series, watch } = require("gulp");
+const rm = require('gulp-rm');
+const sass = require('gulp-sass')(require('node-sass'));
+const concat = require('gulp-concat');
+ 
 task('clean', () => {
-	return src('dist/**/*', {read: false}).pipe(rm())
+ 	return src('dist/**/*', { read: false })
+   	.pipe(rm())
 })
-
+ 
 task('copy', () => {
-	return src('src/**/*.scss').pipe(dest('dist'))
+ 	return src('src/styles/*.scss').pipe(dest('dist'));
 })
-
+ 
 task('styles', () => {
-	return src('src/styles/main.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(dest('dist'))
-})
+ 	return src(['node_modules/normalize.css/normalize.css', 'src/styles/main.scss'])
+   	.pipe(concat('main.scss'))
+   	.pipe(sass().on('error', sass.logError))
+   	.pipe(dest('dist'));
+});
+ 
+watch('./src/styles/**/*.scss', series('styles'))
 
-task ('default', series('clean', 'styles'))
+task('default', series('clean', 'styles'));
